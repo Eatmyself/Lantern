020d40f3814327f53539df866439dfd5f3b7f8b5
Eatmyself <nagin2358@gmail.com>
2021-03-04 21:03:05 +0800

    Initial Commit

---
+++ Lantern/LanternApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanternApp.swift
+//  Lantern
+//
+//  Created by Eatmyself on 2021/3/4.
+//
+
+import SwiftUI
+
+@main
+struct LanternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
